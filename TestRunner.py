69a184efff92cd047c6eff3dce3d2b084fedebff
ThanhHT <tuanthanh1994@gmail.com>
2025-07-25 15:06:10 +0700

    create new testrunner

---
+++ TestRunner.py
@@ -0,0 +1,18 @@
+from robot import run
+import os
+
+# Define test files or directories
+TEST_PATH = os.path.join("Tests", "LoginTest.robot")
+
+# Optional: Customize output paths
+OUTPUT_DIR = "results"
+LOG_NAME = "log.html"
+REPORT_NAME = "report.html"
+OUTPUT_NAME = "output.xml"
+
+# Run tests
+run(TEST_PATH,
+    outputdir=OUTPUT_DIR,
+    log=LOG_NAME,
+    report=REPORT_NAME,
+    output=OUTPUT_NAME)
